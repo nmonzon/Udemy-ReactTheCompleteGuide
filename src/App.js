7863ea2ef25c52357d197f2a8d856f0aef59f220
Nicolas Monzon <nicolasmonzon0@gmail.com>
2019-06-22 13:40:27 -0300

    functional component as statefull  with useState

--- src/App.js
+++ src/App.js
@@ -1,67 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-   state =
+const App = (props) => {
+   const [personState, setPersonState] = useState(
       {  //{ person: [] is a js object 
         persons: [
           { name: 'Max', age: 28},
           { name: 'Manu', age: 29},
           { name: 'Stepanhie', age: 26}
         ]
-      };
-  
-  //[otherValueState] = useState("This is other value");
+      }
+    );
+  const [otherValueState] = useState("This is other value");
 
 //console.log([statePerson, setPersonState]);
 
-  switchValuesHandler = (newName) => {
-      this.state ({
+  const switchValuesHandler = (newName) => {
+      personState ({
         persons: [
           { name: newName, age: 28},
           { name: 'Manu', age: 29},
           { name: 'Stepanhie', age: 27}
         ],
-        otherValueState: this.otherValueState
+        otherValueState: otherValueState
       })
   }
 
-  nameChangedHandler = (event) =>{
-    this.setState( {
+  const nameChangedHandler = (event) =>{
+    setPersonState( {
       persons: [
         { name: 'Max', age: 28},
         { name: event.target.value, age: 29},
         { name: 'Stepanhie', age: 27}
       ]
-    })
+    });
   }
   
-  render(){
     return(//understanding JSX
     <div className="App">
       <h1>React</h1>
       <p>This is really working!</p>
-      <button onClick={() => this.switchValuesHandler("Maxii!")}>Change Values</button>
+      <button onClick={() => switchValuesHandler("Maxii!")}>Change Values</button>
       <Person 
-        name={this.state.persons[0].name}
-        age={this.state.persons[0].age}
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
       />
       <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.switchValuesHandler.bind(this, "Maxii!!!")}
-        changed={this.nameChangedHandler}>My hobbiees: Racing 
+        name={personState.persons[1].name} 
+        age={personState.persons[1].age}
+        click={switchValuesHandler.bind(this, "Maxii!!!")}
+        changed={nameChangedHandler}>My hobbiees: Racing 
       </Person>
         
       <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age}
+        name={personState.persons[2].name} 
+        age={personState.persons[2].age}
       />
     </div>
     );
-  }
+  
   //return React.createElement('div',{className: 'App'},React.createElement('h1',null, 'Contenido de elementoReact H1'));
 }
 
